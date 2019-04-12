c31643cad05948b79fd0a090db6a1d9a3e0e2474
lidazhi <1257249648@qq.com>
2019-04-12 10:51:10 +0800

    0.1.0

---
+++ Example/Pods/Target Support Files/DZLimaoSDK/DZLimaoSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DZLimaoSDK : NSObject
+@end
+@implementation PodsDummy_DZLimaoSDK
+@end
---
+++ Example/Pods/Target Support Files/Pods-DZLimaoSDK_Example/Pods-DZLimaoSDK_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DZLimaoSDK_Example : NSObject
+@end
+@implementation PodsDummy_Pods_DZLimaoSDK_Example
+@end
